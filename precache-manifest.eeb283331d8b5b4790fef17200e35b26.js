87cfa05cc35a5431015f5a76eb869c567ed88a68
Andreas Bigger <abigger87@gmail.com>
2019-04-01 11:42:01 -0700

    Updates

---
+++ precache-manifest.eeb283331d8b5b4790fef17200e35b26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d315ae1961b9034d014",
+    "url": "/birds/static/css/main.a5f8597c.chunk.css"
+  },
+  {
+    "revision": "9d315ae1961b9034d014",
+    "url": "/birds/static/js/main.ccb276cf.chunk.js"
+  },
+  {
+    "revision": "389a54adb7c2513fffe5",
+    "url": "/birds/static/js/runtime~main.1f55c6a7.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/birds/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "26de7580790e19a7118c05fe521758ec",
+    "url": "/birds/index.html"
+  }
+];
